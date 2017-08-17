8d68935dd574e1c1a287215d567522bb474ba758
Jason Maa <jasonmaa3955@gmail.com>
2017-08-17 19:52:11 -0400

    Add English option (#1)

--- nekoatsume_password.rb
+++ nekoatsume_password.rb
@@ -5,14 +5,20 @@ require 'uri'
 require 'dotenv'
 require 'twitter'
 
-def get_password
-  uri = URI.parse('http://hpmobile.jp/app/nekoatsume/neko_daily.php')
+def get_password (eng)
+  if eng == '-E'
+	uri_str = '_en.php'
+  else
+	uri_str = '.php'
+  end
+  uri = URI.parse('http://hpmobile.jp/app/nekoatsume/neko_daily'+uri_str)
 
   response = Net::HTTP.get_response(uri)
   response.body.split(',')[1].force_encoding('UTF-8')
 end
 
 Dotenv.load
+use_eng = ARGV[0]
 
 client = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
@@ -21,8 +27,9 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
 end
 
+
 # Tweet the password if it's not a duplicate
-password = get_password
+password = get_password(use_eng)
 latest_tweets = client.user_timeline(ENV['TWITTER_USER']).map(&:text)
 
 client.update(password) unless latest_tweets.include?(password)
